06a53b4f5674db0d54ce03fe330bc9c600b2b72c
Sneha-Jambagi06 <snehajambagi2020@gmail.com>
2025-11-27 15:51:26 +0530

    Update Bank.py

--- Bank.py
+++ Bank.py
@@ -1,8 +1,10 @@
 import sys
 if len(sys.argv) == 3:
+    script_name=sys.argv[0]
     balance = float(sys.argv[1])
     deposit = float(sys.argv[2])
 else:
+    script_name=sys.argv[0]
     balance = 100
     deposit = 50
 updated_balance = balance + deposit

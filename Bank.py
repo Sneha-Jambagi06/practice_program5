b26298a27be8254c8d7e186d675fc6ce000a1001
Sneha-Jambagi06 <snehajambagi2020@gmail.com>
2025-11-27 15:46:36 +0530

    Update Bank.py

--- Bank.py
+++ Bank.py
@@ -1,11 +1,9 @@
 import sys
-if len(sys.argv)=3:
-  script_name=sys.argv[0]
-  initial = float(sys.argv[1])
-  deposit = float(sys.argv[2])
+if len(sys.argv) == 3:
+    balance = float(sys.argv[1])
+    deposit = float(sys.argv[2])
 else:
-   updated = initial + deposit
-
-print(f"Initial Balance: {initial}")
-print(f"Deposit Amount: {deposit}")
-print(f"Updated Balance: {updated}")
+    balance = 100
+    deposit = 50
+updated_balance = balance + deposit
+print("Updated Balance:", updated_balance)
